bbc19ec47a41d761b68410fa30542fbe7c2916bf
chifaaa <chifaaachelly@gmail.com>
2019-11-19 13:48:25 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Comment from './comments.js'
 import UsersList  from './userslist'
 import Posts from './posts.js'
-import {BrowserRouter, Route,Switch} from 'react-router-dom'
+import {BrowserRouter, Route} from 'react-router-dom'
 
 
 
@@ -19,18 +19,14 @@ const App = () =>  {
     return (
       <BrowserRouter>
       <div className="user-app">
-
         
-        <main className="user-app-main">
+      <main className="user-app-main">
             
-        <Route exact path='/' component={UsersList }/>            
-        
-        <Switch>
-      
+      <Route exact path='/' component={UsersList }/>            
+       
       <Route exact path="/posts/:id" component={Posts} /> 
       <Route   path='/comment/:id' component={Comment}/>
-        
-      </Switch>      
+             
         </main>
       </div>
       </BrowserRouter>
